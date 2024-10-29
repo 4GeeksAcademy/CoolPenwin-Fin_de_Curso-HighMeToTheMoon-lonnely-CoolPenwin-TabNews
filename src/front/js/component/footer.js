8c1d522aefff359fc5990fe3f4d1b0c91c7b78ba
CoolPenwin <coolPenwin@proton.me>
2024-10-30 00:31:07 +0100

    Update footer.js
    Co-authored-by: Souwg <sansanvzla@gmail.com>

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -10,7 +10,7 @@ export const Footer = () => (
   }}
 >
   <p style={{ margin: 0, fontSize: '1rem' }}> {/* Ajusté el tamaño de fuente a 1rem */}
-      Made with 🍑 by
+      Made with <a style={{ textDecoration: 'none' }} href="https://github.com/Souwg">🍑</a> by
       <a
           href="https://github.com/CoolPenwin"
           style={{
